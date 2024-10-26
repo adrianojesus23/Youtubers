0ecdc2758d0cdaf76f177506d2684b6f1dc85ab2
Jesus Amado Vaz <12013@LAPTOP-GGDPPIDL>
2024-10-26 17:34:05 +0100

    Flex, Filter and Roo

---
+++ ApiBookks/Data/BooksContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiBookks.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ApiBookks.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
