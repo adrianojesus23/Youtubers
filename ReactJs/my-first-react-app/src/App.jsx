3c90a9d8adecfe74753c068a807743f86927978f
Jesus Amado Vaz <12013@LAPTOP-GGDPPIDL>
2025-06-17 21:24:38 +0100

    Feat: Learn react

--- ReactJs/my-first-react-app/src/App.jsx
+++ ReactJs/my-first-react-app/src/App.jsx
@@ -2,30 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-const Card = ({ title }) => {
+const Card = ({ title , rating, isCool}) => {
     return (
-        <div>
+        <div className="card">
             <h2>{
                 title
             }</h2>
         </div>
     )
 }
-const Button = () =>{
-    return (
-        <button>Click me</button>
-    )
-}
 
 const App = () => {
 
     return (
-       <div>
-           <h2>Functional Arrow Component</h2>
-           <Card title="Star Wars"/>
+       <div className="card-container">
+           <Card title="Star Wars" rating={5} isCool={true}/>
            <Card title="Avatar" />
            <Card title="The Lion King" />
-           <Button />
        </div>
     )
 }
